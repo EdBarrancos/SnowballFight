b4266be5a9f82078e9d3808441c7794a13013c28
Eduardo Barrancos <47123090+EdBarrancos@users.noreply.github.com>
2022-01-11 14:25:05 +0000

    Working on replit

--- src/main.py
+++ src/main.py
@@ -2,21 +2,25 @@ import asyncio
 import discord
 from discord.ext import commands
 import logging
-from decouple import config
+
+# from decouple import config
+
+import os
 
 #Local Imports
 from bot import MyBot
+import keep_alive
 
 
-logging.basicConfig(format='%(levelname)s:%(message)s', 
-                    filename="../bot_logs/snowman.log", 
-                    level=logging.DEBUG, filemode="w")
+# logging.basicConfig(format='%(levelname)s:%(message)s', 
+#                    filename="../bot_logs/snowman.log", 
+#                    level=logging.DEBUG, filemode="w")
 
 
 myBot = MyBot(("!", "$"))
 
-# keep_alive.keep_alive()
+keep_alive.keep_alive()
 
-# myBot.run(os.getenv('token'))
+myBot.run(os.getenv('token'))
 
-myBot.run(config("token"))
+# myBot.run(config("token"))
